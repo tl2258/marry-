5b27ed52bffa298337bec7d03bdd397a71d0d1af
谭浪a <10582889+tan-langa@user.noreply.gitee.com>
2026-07-30 14:51:22 +0800

    feat: optimize map navigation by jumping directly to Tencent Maps H5 to fix URL garbled text and support native WeChat map selection

--- src/config.js
+++ src/config.js
@@ -11,13 +11,7 @@ export const weddingConfig = {
     hotelName: "娄底市温德姆星铂丽宴会艺术中心",
     address: "娄底市娄星区西贸街与氐星路交汇处温德姆酒店4层",
     latitude: 27.7356,
-    longitude: 111.9945,
-    // 高德地图直接跳转 URL
-    amapUrl: "https://uri.amap.com/marker?position=111.9945,27.7356&name=%E5%A9%84%E5%BA%95%E5%B8%82%E6%B8%A9%E5%BE%B7%E5%A7%86%E6%98%9F%E9%95%B9%E4%B8%BD%E5%AE%B4%E4%BC%9A%E8%87%BA%E6%9C%AF%E4%B8%AD%E5%BF%83&src=wedding_h5&callnative=1",
-    // 百度地图直接跳转 URL
-    baiduMapUrl: "http://api.map.baidu.com/marker?location=27.7356,111.9945&title=%E5%A9%84%E5%BA%95%E5%B8%82%E6%B8%A9%E5%BE%B7%E5%A7%86%E6%98%9F%E9%95%B9%E4%B8%BD%E5%AE%B4%E4%BC%9A%E8%87%BA%E6%9C%AF%E4%B8%AD%E5%BF%83&content=%E5%A9%84%E5%BA%95%E5%B8%82%E5%A9%84%E6%98%9F%E5%8C%BA%E8%A5%BF%E8%B4%B8%E8%A1%97%E4%B8%8E%E6%B0%90%E6%98%9F%E8%B7%AF%E4%BA%A4%E6%B1%87%E5%A4%84%E6%B8%A9%E5%BE%B7%E5%A7%86%E9%85%92%E5%BA%974%E5%B1%82&output=html&src=webapp.baidu.wedding",
-    // 腾讯 / 微信地图直接跳转 URL
-    qqMapUrl: "https://apis.map.qq.com/uri/v1/marker?marker=coord:27.7356,111.9945;title:%E5%A9%84%E5%BA%95%E5%B8%82%E6%B8%A9%E5%BE%B7%E5%A7%86%E6%98%9F%E9%95%B9%E4%B8%BD%E5%AE%B4%E4%BC%9A%E8%87%BA%E6%9C%AF%E4%B8%AD%E5%BF%83;addr:%E5%A9%84%E5%BA%95%E5%B8%82%E5%A9%84%E6%98%9F%E5%8C%BA%E8%A5%BF%E8%B4%B8%E8%A1%97%E4%B8%8E%E6%B0%90%E6%98%9F%E8%B7%AF%E4%BA%A4%E6%B1%87%E5%A4%84%E6%B8%A9%E5%BE%B7%E5%A7%86%E9%85%92%E5%BA%974%E5%B1%82&referer=wedding_h5"
+    longitude: 111.9945
   },
 
   // 3. 微信推送配置 (0费用)
